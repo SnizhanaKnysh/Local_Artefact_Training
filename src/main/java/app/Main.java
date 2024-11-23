74ab50b66b95c8779e40e0a255e97b918d7625ce
Snizhana_K <snizhana.knysh@intellecteu.com>
2024-11-23 10:31:26 +0200

    add implemented task for HW 23.1

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("GENERATING PASSWORD");
+        System.out.println("generated pass: " + PasswordGenerator.generatePassword(16));
+    }
+
+}
